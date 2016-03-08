6f38909fe135c7b27bc06a0659f94001255788b4
Sumanth Srinivasan <sumanth.srinivasan@gmail.com>
2016-03-08 16:49:10 -0500

    bit quantization files

--- week2/bitQuantize.m
+++ week2/bitQuantize.m
@@ -1,18 +1,23 @@
 function [ y ] = bitQuantize( x,quant )
-%UNTITLED6 Summary of this function goes here
-%   Detailed explanation goes here
+%bitQuantize Quantizes a given audio file to values within given number of
+%bits
+% Arguments:
+% x - the input audio file
+% quant - number of bits by which the audio needs to be quantized
+%%
 
+%Create a vector of all possible values with the given no. of bits
 bitVal = [0:(2^quant)-1];
-bitVal = bitVal/bitVal(length(bitVal));
+bitVal = bitVal/bitVal(length(bitVal)); % Normalize them
 y = zeros(1,length(x));
 
 % For every sample in the audio
 for k = 1:length(x);
-    % For every quantization value you can have for n bits
+    % For every quantization value you can have
     for bit = 1:length(bitVal)-1
         
         %If a given sample value falls between two quantization values,
-        %pick the higher one instead of the sample.
+        %pick the higher one instead of the sample itself.
         if abs(x(k)) >= bitVal(bit) && abs(x(k)) <= bitVal(bit+1)
             y(k) = bitVal(bit+1)*(x(k)/abs(x(k)));
         else
@@ -21,6 +26,6 @@ for k = 1:length(x);
         
     end
 end
-figure;hold on; plot(x); plot(y); hold off;
+% figure;hold on; plot(x); plot(y); hold off;
 end
 
---
+++ week2/bit_quantize_3.m
@@ -0,0 +1,6 @@
+% bit-quantization
+% Write a program that can quantize an input audio signal by taking two arguments ?
+% the signal and bit resolution. For example quantizer(x, 14). (5 pts.)
+% Plot the original signal, new quantized signal, and the error. This is sometimes also
+% referred to as bit crunching.
+
